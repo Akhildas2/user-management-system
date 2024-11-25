12a77ae8709cebd84769604ebd098cbf799dae4f
Akhildas2 <akhildasaki12@gmail.com>
2024-11-25 19:14:27 +0530

    removed contents

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/components/header/header.component";
+import { LoginComponent } from './features/auth/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet,LoginComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
